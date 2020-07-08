ac6448c9435fd7461b0a7fec46841560b2e4ccfd
zzaq <az917r@gmail.com>
2020-07-08 15:05:26 +0300

    New Servises

--- src/js/main.js
+++ src/js/main.js
@@ -33,6 +33,24 @@ $(function () {
 			document.getElementById(`toggle`).checked = false
 		})
 
+		// SVG hack
+		$('img.svg').each(function(){
+			var $img = $(this);
+			var imgClass = $img.attr('class');
+			var imgURL = $img.attr('src');
+			$.get(imgURL, function(data) {
+			  var $svg = $(data).find('svg');
+			  if(typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass+' replaced-svg');
+			  }
+			  $svg = $svg.removeAttr('xmlns:a');
+			  if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+				$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+			  }
+			  $img.replaceWith($svg);
+			}, 'xml');
+		  });
+
 		// Jentle Scroll
 		$(`.menu, .menu-mobile, .logo-mobile, .logo-place`).on(`click`, `a[href^="#"]`, function (event) {
 			event.preventDefault()
